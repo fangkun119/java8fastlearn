aefc8389a9a7926416086f91e5d7ca30b696a0cc
fangkun119 <fangkun119@hotmail.com>
2020-09-26 14:09:16 +0800

    CH1.6: Default Method

--- code/ch1/sec07/DefaultMethods.java
+++ code/ch1/sec07/DefaultMethods.java
@@ -1,13 +1,28 @@
+/**
+* 默认方法：
+* 1. Java 8开始，可以给接口添加一个默认方法(default method)
+*    是为了满足诸为Collection添加forEach方法、同时有不用修改各种实现Collection接口的容器类这样的需求
+* 2. 默认方法的出现，使得Collection/AbstractCollection, WindowListener/WindowAdaptor这样的经典模式不再想之前那样那么需要
+*    不再需要一个抽象类来实现大部分或全部代码，也可以实现在接口中
+* 3. 命名冲突时应当如何处理？
+*    父类、与接口出现同名函数：使用父类的函数
+*    两个接口出现同名函数（不论是不是默认函数）：都会编译期报错，需要由实现类提供该函数的代码
+*/
+
 interface Person {
    long getId();
+   // 接口默认函数getName
    default String getName() { return "John Q. Public"; }
 }
 
 interface Persistent {
+   // 接口默认函数getName
    default String getName() { return getClass().getName() + "_" + hashCode(); }
 }
 
 class Student implements Person, Persistent {
    public long getId() { return 42; }
+   // 发生了函数命名冲突，冲突来自两个接口，由实现类提供该函数的代码
+   // Person.super.getName()表示调用接口Person的默认函数getName()
    public String getName() { return Person.super.getName(); }
 }
