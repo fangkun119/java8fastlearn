ddc910a78e99cddc53b2c71bd3dfa16e9c948477
fangkun119 <fangkun119@hotmail.com>
2020-09-26 13:15:04 +0800

    CH1.5 Variable Scope

--- code/ch1/sec06/Application.java
+++ code/ch1/sec06/Application.java
@@ -1,8 +1,23 @@
 import java.nio.file.*;
 import java.util.*;
 
+/**
+* 变量作用域：
+* 1. Lambda表达式可以捕获闭合作用域的变量值，并且使用这些值
+* 2. Lambda表达式强制约束，不可以修改这些所使用的局部变量
+*    是因为Lambda访问这些变量的原理、类似于变量值拷贝、为了避免两个值不一致问题的发生（特别是多线程环境），增加了这个约束
+* 3. 即便是增加了不可修改约束，仍然不能完全避免“不一致”问题的发生
+*    (1) 类成员变量、静态变量不受约束（只约束局部变量）
+*    (2) 如果变量是一个对象，不可修改约束只能保证这个变量不指向其他对象，不能阻挡对象内部的修改
+*    彻底的解决方法、需要使用stream
+* 4. Lambda表达式的方法体、与嵌套代码块有相同的作用域，适用同样的命名和屏蔽规则
+* 5. Lambda表达式中使用this关键字时，会引用创建该表达式的方法的this参数（而不是执行该表达式的方法的this参数）
+*/
+
 public class Application {
    public static void main(String[] args) {
+
+      // Lambda表达式的方法体、与嵌套代码块有相同的作用域，适用同样的命名和屏蔽规则
 
       // Path first = Paths.get("/usr/bin");
       // Uncomment to see error "variable first is already defined"
@@ -15,6 +30,8 @@ public class Application {
    }
 
    public void doWork() {      
+      // Lambda表达式中使用this关键字时，会引用创建该表达式的方法的this参数（而不是执行该表达式的方法的this参数）
+
       Runnable runner = () -> { System.out.println(this.toString()); };
       runner.run();
       // Prints Application@... since this refers to an Application object
--- code/ch1/sec06/VariableScope.java
+++ code/ch1/sec06/VariableScope.java
@@ -4,12 +4,26 @@ import java.nio.file.*;
 import java.util.*;
 import java.util.stream.*;
 
+/**
+* 变量作用域：
+* 1. Lambda表达式可以捕获闭合作用域的变量值，并且使用这些值
+* 2. Lambda表达式强制约束，不可以修改这些所使用的局部变量
+*    是因为Lambda访问这些变量的原理、类似于变量值拷贝、为了避免两个值不一致问题的发生（特别是多线程环境），增加了这个约束
+* 3. 即便是增加了不可修改约束，仍然不能完全避免“不一致”问题的发生
+*    (1) 类成员变量、静态变量不受约束（只约束局部变量）
+*    (2) 如果变量是一个对象，不可修改约束只能保证这个变量不指向其他对象，不能阻挡对象内部的修改
+*    彻底的解决方法、需要使用stream
+* 4. Lambda表达式的方法体、与嵌套代码块有相同的作用域，适用同样的命名和屏蔽规则
+* 5. Lambda表达式中使用this关键字时，会引用创建该表达式的方法的this参数（而不是执行该表达式的方法的this参数）
+*/
+
 public class VariableScope {
    public static void main(String[] args) {
       repeatMessage("Hello", 100);
    }
 
    public static void repeatMessage(String text, int count) {
+      // lambda表达式引用作用域中的变量text和count
       Runnable r = () -> {
          for (int i = 0; i < count; i++) {
             System.out.println(text);
@@ -19,8 +33,8 @@ public class VariableScope {
       new Thread(r).start();                  
    }
 
-   public static void repeatMessage2(String text, int count)
-   {
+   public static void repeatMessage2(String text, int count) {
+      // lambda表达式访问局部变量时，不可以修改这些变量
       Runnable r = () -> {
          while (count > 0) {
             // count--; // Error: Can't mutate captured variable
@@ -33,6 +47,7 @@ public class VariableScope {
 
    public static void countMatches(Path dir, String word) throws IOException {
       Path[] files = getDescendants(dir);
+      // lambda表达式访问局部变量时，不可以修改这些变量
       int matches = 0;
       for (Path p : files) 
          new Thread(() -> { if (contains(p, word)) {
@@ -43,10 +58,11 @@ public class VariableScope {
    */
 
    private static int matches;
-
+   
    public static void countMatches2(Path dir, String word) {
       Path[] files = getDescendants(dir);
       for (Path p : files) 
+         // lambda表达式访问类成员变量、静态变量时，编译器不会阻止对这些变量的修改，但是要小心带来的线程安全问题
          new Thread(() -> { if (contains(p, word)) {
                   matches++; 
                   // CAUTION: Legal to mutate matches, but not threadsafe      
@@ -56,6 +72,7 @@ public class VariableScope {
    // Warning: Bad code ahead
    public static List<Path> collectMatches(Path dir, String word) {
       Path[] files = getDescendants(dir);
+      // 当局部变量是对象时，lambda表达式仅仅不能修改这些对象的引用，但是仍然可以修改对象的数据，要小心带来的安全问题
       List<Path> matches = new ArrayList<>();
       for (Path p : files) 
          new Thread(() -> { if (contains(p, word)) {
