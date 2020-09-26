7a8dad06710921625163abc5ef21b6ffa41c9a2b
fangkun119 <fangkun119@hotmail.com>
2020-09-26 17:00:06 +0800

    CH8.1 Improvement of String by Java 8

--- code/ch8/sec01/Strings.java
+++ code/ch8/sec01/Strings.java
@@ -1,9 +1,20 @@
 import java.time.*;
 
+/**
+* Java 8为String增加了静态方法String.join(sep, ...)，即通过一个分隔符将一组字符串拼接起来
+* 被拼接的字符串可以用三种方式传入：
+* 1. 可变参数列表
+* 2. 数组
+* 3. Iterable<? extends CharSequences>对象，例如ZoneId.getAvailableZoneIds()返回的Set<String>
+*/
+
 public class Strings {
    public static void main(String[] args) {
+      // 拼接可变参数列表中的字符串
       String joined = String.join("/", "usr", "local", "bin"); // "usr/local/bin"
       System.out.println(joined);
+
+      // 拼接Set<String>中的字符串
       String ids = String.join(", ", ZoneId.getAvailableZoneIds());
       System.out.println(ids);
    }
