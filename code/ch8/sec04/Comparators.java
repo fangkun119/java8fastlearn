2cf7212b072a9428e9bd60939f5e96e63cd9552e
fangkun119 <fangkun119@hotmail.com>
2020-09-26 19:59:03 +0800

    CH8.4 Collection Methods

--- code/ch8/sec04/Comparators.java
+++ code/ch8/sec04/Comparators.java
@@ -65,25 +65,45 @@ public class Comparators
       new Person("Barack Hussein Obama"),
    };
 
+
+
+   /**
+   * 比较器：
+   * 1. Comparator.comparing 静态方法提供的比较器，接收一个“Key Extractor”，返回一个Comparator对象
+   * 2. Comparator.thenComparing与Comparator.comparing结合使用，来进行多级比较
+   * 3. Comparator.comparing可接收一个key比较器（下面的lambda语句），指定如何比较被“Key Extractor”取出来的key
+   * 4. Comparator.comparingInt/Long/Double方法，用来避免int/long/double的装箱、拆箱
+   * 5. nullsFirst(naturalOrder())或nullsLast(naturalOrder())来让null值排在最前或最后，而不是抛异常
+   * 6. reversed()用来颠倒任何一个比较器，例如natrualOrder().reversed()等价于reverseOrder()
+   */ 
+
    public static void main(String[] args) {
+      
+      // Comparator.comparing 静态方法提供的比较器，接收一个“Key Extractor”，返回一个Comparator对象
       Arrays.sort(presidents, Comparator.comparing(Person::getName));
       System.out.println(Arrays.toString(presidents));
 
+      // Comparator.thenComparing与Comparator.comparing结合使用，来进行多级比较
       Arrays.sort(presidents,
-         Comparator.comparing(Person::getLastName)
-         .thenComparing(Person::getFirstName));
+         Comparator
+            .comparing(Person::getLastName)
+            .thenComparing(Person::getFirstName));
       System.out.println(Arrays.toString(presidents));
 
-      Arrays.sort(presidents, Comparator.comparing(Person::getName,
-            (s, t) -> Integer.compare(s.length(), t.length())));
+      // Comparator.comparing可接收一个key比较器（下面的lambda语句），指定如何比较被“Key Extractor”取出来的key
+      Arrays.sort(presidents, 
+         Comparator.comparing(Person::getName, (s, t) -> Integer.compare(s.length(), t.length())));
       System.out.println(Arrays.toString(presidents));
 
+      // Comparator.comparingInt/Long/Double方法，用来避免int/long/double的装箱、拆箱
       Arrays.sort(presidents, Comparator.comparingInt(p -> p.getName().length()));
       System.out.println(Arrays.toString(presidents));
 
+      // nullsFirst(naturalOrder())或nullsLast(naturalOrder())来让null值排在最前或最后，而不是抛异常
       Arrays.sort(presidents, Comparator.comparing(Person::getMiddleName, nullsFirst(naturalOrder())));
       System.out.println(Arrays.toString(presidents));
 
+      // reversed()用来颠倒任何一个比较器，例如natrualOrder().reversed()等价于reverseOrder()
       Arrays.sort(presidents, Comparator.comparing(Person::getMiddleName, nullsFirst(Comparator.<String>naturalOrder().reversed())));
       System.out.println(Arrays.toString(presidents));
    }
