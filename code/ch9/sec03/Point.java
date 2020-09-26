bd2e9bc376bb53b5e14b7f3c114d994045c97c40
fangkun119 <fangkun119@hotmail.com>
2020-09-26 16:48:36 +0800

    CH9.3 support for equals, hashcode and compareTo methods from Java 7

--- code/ch9/sec03/Point.java
+++ code/ch9/sec03/Point.java
@@ -1,7 +1,13 @@
+/**
+* Java 7 提供API，用来简化编写equals, hashCode, compareTo的代码
+*/
+
 public class Point {
    private int x, y;
-
    public int compareTo(Point other) {
+      // 使用Java 7提供的方法，可以避免直接相减带来的溢出问题
+      // 除了Integer
+      // Long、Short、Byte、Boolean都提供了各自的静态方法compare
       int diff = Integer.compare(x, other.x); // No risk of overflow
       if (diff != 0) return diff;
       return Integer.compare(y, other.y);
